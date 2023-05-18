c5a7bc1904cce39fba6b75d2f39b0bbaf7e2ce95
sonbuwon <foflzld3387@gmail.com>
2023-05-18 18:19:44 +0900

    vue.js tutorial 1

---
+++ jsforVue.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello Vue!",
+  },
+});
+
+var app2 = new Vue({
+  el: "#app-2",
+  data: {
+    message: "이 페이지는 " + new Date() + "에 로드 되었습니다",
+  },
+});
+
+var app3 = new Vue({
+  el: "#app-3",
+  data: {
+    seen: false,
+  },
+});
+
+var app4 = new Vue({
+  el: "#app-4",
+  data: {
+    todos: [{ text: "JS" }, { text: "Vue" }, { text: "do awesome something" }],
+  },
+});
